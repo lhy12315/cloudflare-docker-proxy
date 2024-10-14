c7629c98fb40a5b815795116733b4f853fb3fe42
lhy12315 <46898773+lhy12315@users.noreply.github.com>
2024-10-14 17:45:09 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.lixd.xyz": dockerHub,
-  "quay.lixd.xyz": "https://quay.io",
-  "gcr.lixd.xyz": "https://gcr.io",
-  "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  "k8s.lixd.xyz": "https://registry.k8s.io",
-  "ghcr.lixd.xyz": "https://ghcr.io",
-  "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
+  "docker.swift12315.us.kg": dockerHub,
+  "quay.swift12315.us.kg": "https://quay.io",
+  "gcr.swift12315.us.kg": "https://gcr.io",
+  "k8s-gcr.swift12315.us.kg": "https://k8s.gcr.io",
+  "k8s.swift12315.us.kg": "https://registry.k8s.io",
+  "ghcr.swift12315.us.kg": "https://ghcr.io",
+  "cloudsmith.swift12315.us.kg": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.lixd.xyz": dockerHub,
+  "docker-staging.swift12315.us.kg": dockerHub,
 };
 
 function routeByHosts(host) {
