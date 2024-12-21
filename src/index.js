bcb0a85f1eddafd38a155eb394e77f56a0633d5a
lhy12315 <46898773+lhy12315@users.noreply.github.com>
2024-12-22 03:02:57 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,13 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
-
-  // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker.swift12315.us.kg": "https://registry-1.docker.io",
+  "quay.swift12315.us.kg": "https://quay.io",
+  "gcr.swift12315.us.kg": "https://gcr.io",
+  "k8s-gcr.swift12315.us.kg": "https://k8s.gcr.io",
+  "k8s.swift12315.us.kg": "https://registry.k8s.io",
+  "ghcr.swift12315.us.kg": "https://ghcr.io",
+  "cloudsmith.swift12315.us.kg": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
